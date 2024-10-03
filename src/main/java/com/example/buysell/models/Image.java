ebba3143d13154a4c54a0618cc9ab09e37c29e13
sokolovNV <n.sockolow2016@yandex.ru>
2024-10-03 09:35:20 +0300

    first commit

---
+++ src/main/java/com/example/buysell/models/Image.java
@@ -0,0 +1,54 @@
+package com.example.buysell.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String originalFileName;
+    private Long size;
+    private String contentType;
+    private boolean previewImage;
+    @Lob
+    private byte[] bytes;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Product product;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOriginalFileName(String originalFileName) {
+        this.originalFileName = originalFileName;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public void setPreviewImage(boolean previewImage) {
+        this.previewImage = previewImage;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
